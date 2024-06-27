a50d69a9e4bb07971f336ab9ba32de8655fab845
Kim JaeMin <2098kjm@naver.com>
2024-06-27 15:53:28 +0900

    [Refactor] Feign 사용 X, DB 공유로 성능 향상

--- src/main/java/com/example/pensionMatching/service/PensionMatchingServiceImpl.java
+++ src/main/java/com/example/pensionMatching/service/PensionMatchingServiceImpl.java
@@ -10,16 +10,19 @@ import com.example.pensionMatching.domain.entity.PensionBonusNum;
 import com.example.pensionMatching.domain.entity.PensionWinNum;
 import com.example.pensionMatching.domain.entity.PurchasedTickets;
 import com.example.pensionMatching.domain.repository.PurchasedTicketsRepository;
+import com.example.pensionMatching.kafka.dto.KafkaStatus;
 import com.example.pensionMatching.kafka.producer.KafkaProducer;
+import jakarta.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 
-public class PensionMatchingServiceImpl implements PensionMatchingService, TicketService{
+public class PensionMatchingServiceImpl implements PensionMatchingService, TicketService {
 
     private final ApiWinDraw apiWinDraw;
     private final ApiPayment apiPayment;
@@ -27,13 +30,22 @@ public class PensionMatchingServiceImpl implements PensionMatchingService, Ticke
     private final KafkaProducer kafkaProducer;
 
     @Override
+    public void insertTickets(List<PurchasedTickets> purchasedTickets) {
+        purchasedTicketsRepository.saveAll(purchasedTickets);
+    }
+
+    @Override
+    @Transactional
     public void matchingTicket(PensionWinAndBonus drawResult) {
-        List<PurchasedTickets> purchasedTickets =
-            apiWinDraw.getPurchasedTickets(drawResult.pensionWinNum().getDrawRound());
+        List<PurchasedTickets> purchasedTickets = purchasedTicketsRepository.findByRound(
+            drawResult.pensionWinNum().getDrawRound());
+        // apiWinDraw.getPurchasedTickets(drawResult.pensionWinNum().getDrawRound());
 
-        List<PurchasedTickets> ticketsList = ticketWinMatching(drawResult, purchasedTickets);
-
-        purchasedTicketsRepository.saveAll(ticketsList);
+        purchasedTickets.forEach((purchasedTicket) -> {
+            Integer result = ticketWinMatching(drawResult, purchasedTicket);
+            purchasedTicket.setResult(result);
+            purchasedTicket.setDrawDate(drawResult.pensionWinNum().getDrawDate());
+        });
     }
 
     @Override
@@ -43,79 +55,80 @@ public class PensionMatchingServiceImpl implements PensionMatchingService, Ticke
 
     @Override
     public List<TicketResult> getAllTicketByResult(String userId, Integer result) {
-        if(result > 0) {
+        if (result > 0) {
             return purchasedTicketsRepository.findByUserIdAndResultGreaterThan(userId, 0);
         } else {
             return purchasedTicketsRepository.findByUserIdAndResultIs(userId, 0);
         }
     }
 
-    @Override
-    public List<PurchasedTickets> ticketWinMatching(PensionWinAndBonus drawResult, List<PurchasedTickets> purchasedTickets) {
-        for(PurchasedTickets purchasedTicket : purchasedTickets) {
-            Integer result = 0; // 미추첨
-            purchasedTicket.setDrawDate(drawResult.pensionWinNum().getDrawDate());
 
-            if(purchasedTicket.getSixth() == drawResult.pensionWinNum().getSixthNum()){
-                if(purchasedTicket.getFifth() == drawResult.pensionWinNum().getFifthNum()){
-                    if(purchasedTicket.getFourth() == drawResult.pensionWinNum().getFourthNum()){
-                        if(purchasedTicket.getThird() == drawResult.pensionWinNum().getThirdNum()){
-                            if(purchasedTicket.getSecond() == drawResult.pensionWinNum().getSecondNum()){
-                                if(purchasedTicket.getFirst() == drawResult.pensionWinNum().getFirstNum()){
-                                    if(purchasedTicket.getGroupNum() == drawResult.pensionWinNum().getGroupNum()){
-                                        result = 1;
-                                    } else {
-                                        result = 2;
-                                    }
+    @Override
+    public Integer ticketWinMatching(PensionWinAndBonus drawResult,
+        PurchasedTickets purchasedTicket) {
+        Integer result = 0; // 미추첨
+        purchasedTicket.setDrawDate(drawResult.pensionWinNum().getDrawDate());
+
+        if (purchasedTicket.getSixth() == drawResult.pensionWinNum().getSixthNum()) {
+            if (purchasedTicket.getFifth() == drawResult.pensionWinNum().getFifthNum()) {
+                if (purchasedTicket.getFourth() == drawResult.pensionWinNum().getFourthNum()) {
+                    if (purchasedTicket.getThird() == drawResult.pensionWinNum().getThirdNum()) {
+                        if (purchasedTicket.getSecond() == drawResult.pensionWinNum()
+                            .getSecondNum()) {
+                            if (purchasedTicket.getFirst() == drawResult.pensionWinNum()
+                                .getFirstNum()) {
+                                if (purchasedTicket.getGroupNum() == drawResult.pensionWinNum()
+                                    .getGroupNum()) {
+                                    result = 1;
                                 } else {
-                                    result = 3;
+                                    result = 2;
                                 }
                             } else {
-                                result = 4;
+                                result = 3;
                             }
                         } else {
-                            result = 5;
+                            result = 4;
                         }
                     } else {
-                        result = 6;
+                        result = 5;
                     }
                 } else {
-                    result = 7;
+                    result = 6;
                 }
-            }else {
-                result = -1;
+            } else {
+                result = 7;
             }
-
-            if(result == -1){
-                result = ticketBonusMatching(drawResult.pensionBonusNum(), purchasedTicket, result);
-            }
-
-            purchasedTicket.setResult(result);
-
-            if(result > 0) {
-                // KafkaUserDto kafkaUserDto = new KafkaUserDto(purchasedTicket.getUserId(),
-                KafkaUserDto kafkaUserDto = new KafkaUserDto("11111111-1111-1111-1111-111111111111",
-                    getPrize(result), "연금복권", result);
-                kafkaProducer.send(kafkaUserDto, "email-topic");
-
-                // apiPayment.winResult(purchasedTicket.getUserId(), result, getPrize(result));
-                // apiPayment.winResult("11111111-1111-1111-1111-111111111111", result, getPrize(result));
-            }
+        } else {
+            result = -1;
         }
 
-        return purchasedTickets;
+        if (result == -1) {
+            result = ticketBonusMatching(drawResult.pensionBonusNum(), purchasedTicket, result);
+        }
+
+        if (result > 0) {
+            // KafkaUserDto kafkaUserDto = new KafkaUserDto(purchasedTicket.getUserId(),
+            KafkaUserDto kafkaUserDto = new KafkaUserDto("11111111-1111-1111-1111-111111111111",
+                getPrize(result), "연금복권", result);
+            kafkaProducer.send(kafkaUserDto, "email-topic");
+
+            // apiPayment.winResult(purchasedTicket.getUserId(), result, getPrize(result));
+            // apiPayment.winResult("11111111-1111-1111-1111-111111111111", result, getPrize(result));
+        }
+
+        return result;
     }
 
     @Override
     public Integer ticketBonusMatching(PensionBonusNum pensionBonusNum,
         PurchasedTickets purchasedTicket, Integer result) {
-        if(purchasedTicket.getSixth() == pensionBonusNum.getSixthNum() &&
+        if (purchasedTicket.getSixth() == pensionBonusNum.getSixthNum() &&
             purchasedTicket.getFifth() == pensionBonusNum.getFifthNum() &&
             purchasedTicket.getFourth() == pensionBonusNum.getFourthNum() &&
             purchasedTicket.getThird() == pensionBonusNum.getThirdNum() &&
             purchasedTicket.getSecond() == pensionBonusNum.getSecondNum() &&
             purchasedTicket.getFirst() == pensionBonusNum.getFirstNum()
-        ){
+        ) {
             result = 8;
         }
         return result;
@@ -137,4 +150,5 @@ public class PensionMatchingServiceImpl implements PensionMatchingService, Ticke
             return 1_000L;
         return 0L;
     }
+
 }
